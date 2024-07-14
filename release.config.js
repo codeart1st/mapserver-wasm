0463213fe6e9addf5a2a1bcd62f3bb48ae22d2e9
Paul Lange <paul.lange@data-experts.de>
2024-07-14 18:13:17 +0200

    chore: upgrade npm dependencies - regenerate lockfile

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
-    '@semantic-release/gitlab',
+    '@semantic-release/github',
     [
       '@semantic-release/git',
       {
