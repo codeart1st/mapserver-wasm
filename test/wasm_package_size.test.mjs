b5fadfd0080faba05c87d500c85e13a6c2c0312f
Paul Lange <paul.lange@data-experts.de>
2025-01-18 15:52:11 +0100

    feat: upgrade emsdk to 4.0.1

--- test/wasm_package_size.test.mjs
+++ test/wasm_package_size.test.mjs
@@ -5,6 +5,6 @@ const THRESHOLD = 10240
 test('mapserver.wasm file should not be larger than ~2.9MB with brotli compression', () => {
   const stats = fs.statSync('./dist/mapserver.wasm.br')
 
-  expect(stats.size).toBeLessThanOrEqual(2969672 + THRESHOLD) // we should avoid getting bigger than that
+  expect(stats.size).toBeLessThanOrEqual(2988783 + THRESHOLD) // we should avoid getting bigger than that
   expect(stats.size).toBeGreaterThan(2000000) // if we got to small, maybe something is wrong?
 })
