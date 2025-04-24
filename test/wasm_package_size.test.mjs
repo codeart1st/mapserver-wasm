6b921916d44cd651f196dde7895e2496eade7f07
Paul Lange <paul.lange@data-experts.de>
2025-04-24 07:53:59 +0200

    chore: increase allowed package size once again

--- test/wasm_package_size.test.mjs
+++ test/wasm_package_size.test.mjs
@@ -5,6 +5,6 @@ const THRESHOLD = 10240
 test('mapserver.wasm file should not be larger than ~3.1MB with brotli compression', () => {
   const stats = fs.statSync('./dist/mapserver.wasm.br')
 
-  expect(stats.size).toBeLessThanOrEqual(3077552 + THRESHOLD) // we should avoid getting bigger than that
+  expect(stats.size).toBeLessThanOrEqual(3091361 + THRESHOLD) // we should avoid getting bigger than that
   expect(stats.size).toBeGreaterThan(2000000) // if we got to small, maybe something is wrong?
 })
